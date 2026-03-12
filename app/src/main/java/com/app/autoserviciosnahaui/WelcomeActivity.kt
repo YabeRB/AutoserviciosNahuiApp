120ec63c485b74aafe183ec66a3c4b2579419a19
YabeRB <yaberb@hotmail.com>
2026-03-12 13:49:12 -0500

    Primer commit de Login, Registro y Bienvenida

---
+++ app/src/main/java/com/app/autoserviciosnahaui/WelcomeActivity.kt
@@ -0,0 +1,26 @@
+package com.app.autoserviciosnahaui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        val login = findViewById<Button>(R.id.btnLogin)
+        val register = findViewById<Button>(R.id.btnRegister)
+
+        login.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        register.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+}
